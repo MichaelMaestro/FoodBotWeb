2297680540e5626dd7abd3ff56cd09ea92d65303
WIN-RLKA6TQVJ6L\User <id25808052@live.com>
2017-06-05 01:19:51 +0300

    Наброски адаптивности

--- index.php
+++ index.php
@@ -15,91 +15,110 @@ if(isset($_COOKIE['Kulik'])){
     <link href="index.css" rel="stylesheet">
     <link href="css/bootstrap.css" rel="stylesheet">
 </head>
-<body> 
-<header class="header">
-    <div class="shell">
-        <a href="index.php">
-            <h1 class="logo">FoodBot</h1>
-        </a>
-        <input type="button" class="btn btn-success" name="reg" value="Регистрация" onclick="window.location =        'registration.php'">
-    </div>  
-</header>
- 
-<div class="block-1">
-    <div class="content">
-        <h1 class="hero-unit">
-            <a href="https://web.telegram.org/#/im?p=@takethefood_bot" target="_blank">Чат-бот</a> для<br>вашего ресторана
-        </h1>
-        <div class="login"> 
-            <form class="form-horizontal" action="main.php" method="post">
-                <legend>Авторизация</legend>
-                <p><input class="input" type="text" name="login" value="" placeholder="Логин или Email" required></p>
-                <p><input type="password" name="pass" value="" placeholder="Пароль" required></p>
-                <p><a  href="pass_refresh.php" id="refresh" >Забыли пароль?</a></p>
-                <p class="remember_me">    
-                    <input type="checkbox" id="remember_me" name="remember_me">
-                    <label for="remember_me">Запомнить меня</label>
-                </p>
-                <p class="submit">
-                    <input type="submit" class="btn btn-success" name="commit" value="Войти">
-                </p>
-            </form>
-        </div>
-    </div>
-</div>
+<body>
 
-<div class="block-2">
-    <div class="content">
-        <div class="text">
-            <h3>Как это работает?</h3>
-            <p>Очень просто!<br>Пользователь заказывает еду у бота, заказ перенапраляется к вам.</p>
-            <br>
+<div id="wrapper">
 
-            <h3>Зачем это нужно?</h3>
-            <p>С помощью чат бота вы сможете получать больше заказов и возможность привлечь новых клиентов.<br>Так же вы получите статистику заказов через бота</p>
-        </div>
-        <div class="picture">
-            <img src="images/ques.png">
-        </div>
+    <div class="container-fluid">
+        <header id="header" class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="col-md-2 col-sm-5">
+                    <a href="index.php">
+                        <h1 class="logo">FoodBot</h1>
+                    </a>
+                </div>
+                <div  class="col-md-10 col-sm-7 col-xs-5">
+                    <input type="button" class="btn btn-success" name="reg" value="Регистрация" onclick="window.location = 'registration.php'">
+                </div>
+            </div>  
+        </header>
     </div>
-</div>
-   
-<div class="block-3">
-    <div class="content">
-        <div class="text">
-            
-            <div class="text_left">
-                <h3>Меню</h3>
-                <p>В личном кабенете вашего ресторана вы сможете вести своё меню,<br>именно с этим меню и будет работать бот</p>
+
+
+    <div class="container-fluid">
+        <div class="block-1 row">
+            <div class="content">
+                <div class="col-md-7 col-sm-12">
+                    <h1 class="hero-unit">
+                        <a href="https://web.telegram.org/#/im?p=@takethefood_bot" target="_blank">Чат-бот</a> для вашего ресторана
+                    </h1>
+                </div>
+                <div id="login" class="col-md-5 col-sm-12"> 
+                    <form class="form-horizontal" action="main.php" method="post">
+                        <legend>Авторизация</legend>
+                        <p><input class="input" type="text" name="login" value="" placeholder="Логин или Email" required></p>
+                        <p><input type="password" name="pass" value="" placeholder="Пароль" required></p>
+                        <p><a  href="pass_refresh.php" id="refresh" >Забыли пароль?</a></p>
+                        <p class="remember_me">    
+                            <input type="checkbox" id="remember_me" name="remember_me">
+                            <label for="remember_me">Запомнить меня</label>
+                        </p>
+                        <p class="submit">
+                            <input type="submit" class="btn btn-success" name="commit" value="Войти">
+                        </p>
+                    </form>
+                </div>
             </div>
+        </div>
+    </div>
 
-            <div class="text_right">
-                <h3>Ключевые слова</h3>
-                <p>Бот осуществляет поиск по ключевым словам, то есть вы выбираете ключевое слово<br> для каждого блюда и бот найдёт это блюдо по этому слову.</p>
+
+    <div class="container-fluid">
+        <div class="block-2 row">
+            <div class="content"> 
+                <div class="text col-md-7 col-sm-6">
+                    <h3>Как это работает?</h3>
+                    <p>Очень просто!<br>Пользователь заказывает еду у бота, заказ перенапраляется к вам.</p>
+                    <br>
+
+                    <h3>Зачем это нужно?</h3>
+                    <p>С помощью чат бота вы сможете получать больше заказов и возможность привлечь новых клиентов.<br>Так же вы получите статистику заказов через бота.</p>
+                </div>
+                <div class="picture col-md-5 col-sm-6">
+                    <img src="images/ques.png">
+                </div>
+
             </div>
+        </div>
+    </div>
 
+
+    <div class="container-fluid">
+        <div class="block-3 row">
+            <div class="content">
+                <div class="text col-md-6 col-sm-6">
+                    <div class="text_left">
+                        <h3>Меню</h3>
+                        <p>В личном кабенете вашего ресторана вы сможете вести своё меню,<br>именно с этим меню и будет работать бот.</p>
+                    </div>
+
+                    <div class="text_left">
+                        <h3>Ключевые слова</h3>
+                        <p>Бот осуществляет поиск по ключевым словам, то есть вы выбираете ключевое слово<br> для каждого блюда и бот найдёт это блюдо по этому слову.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6"></div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container-fluid">
+        <div class="block-4 row">
+            <div class="content">
+                <div class="text col-md-7 col-sm-6">
+                    <h3>Вас будут ждать с нетерпением</h3>
+                    <br>
+                    <p>Наш сервис использует технологию Google maps, благодаря которой вашему клиету будет сообщаться ориентировочное время доставки вашего блюда.</p>
+                </div>
+                <div class="picture col-md-5 col-sm-6">
+                    <img src="images/map-marker.png">
+                </div>
+            </div>
         </div>
     </div>
 </div>
 
-<div class="block-4">
-    <div class="content">
-        <div class="text">
-            <h3>Вас будут ждать с нетерпением</h3>
-            <br>
-            <p>Наш сервис использует технологию Google maps, благодаря которой вашему клиету будет сообщаться ориентировочное время доставки вашего блюда.</p>
-        </div>
-        <div class="picture">
-            <img src="images/map-marker.png">
-        </div>
-    </div>
-</div>
-
-<div class="footer">
-    <div class="content">
-
-    </div>        
-</div>
 <!-- Скритпы -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="js/bootstrap.js"></script>
--- main.php
+++ main.php
@@ -18,17 +18,25 @@ else{
     <title>Личный кабинет</title>
     <link href="style.css" rel="stylesheet">
     <link href="main.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/font-awesome.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css" rel="stylesheet"> 
+    <link href="css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
-<header class="header">
-  <div class="shell">
-    <h1 class="logo">FoodBot</h1>
-    <input type="submit" id="exit" class="btn btn-danger"  value="Выход">
-  </div>
-</header>
+<div id="wrapper">
+
+<div class="container-fluid">
+  <header id="header" class="row">
+    <div class="col-md-12 col-sm-12">
+        <div class="col-md-6 col-sm-5">
+                <h1 class="logo">FoodBot</h1>
+        </div>
+        <div  class="col-md-6 col-sm-7 col-xs-5">
+            <input type="submit" id="exit" class="btn btn-danger"  value="Выход">
+        </div>
+      </div>  
+  </header>
+</div>
 
 <div class="overlay" title="окно"></div>
 <div class="popup">
@@ -39,81 +47,98 @@ else{
   </form>
 </div>
 
-<div class="passport">
-  <h2 class="center-align zag2">Добро пожаловть, <?=$_SESSION['name']?> !</h2>
-  <div class="info">
-    Телефон: <?=$result['phone']?><br>
-    Сайт ресторана: <?=$result['site']?><br>
-    Директор: <?=$result['name_dir']?><br>
-    ИНН:<?=$result['inn']?>
-    ОГРН:<?=$result['ogrn']?>
-    р/с:<?=$result['rs']?><br>
-    <a href="statistic.php"><i class="fa fa-bar-chart" aria-hidden="true"></i>Статистика </a>
-  </div>
-  <div class="licence">
-    <img src="<?=$result['lic']; ?>">
-  </div>
-</div>
-
-<div class="menu">
-  <h2>Меню:</h2>
-  <table>
-    <tr>
-      <th>Наличие блюда</th>
-      <th>Наименование блюда</th>
-      <th>Фото</th>
-      <th>Описание</th>
-      <th>Ингридиенты</th>
-      <th>Цена</th>
-      <th>Ключевые слова</th>
-    </tr>
-
-<?php do{?>
-    <tr>
-      <td><input type="checkbox" id="check" checked></td>
-      <td id="dish_name"><?=$dish['dish_name']?></td>
-      <td><img src="<?=$dish['icons']?>"></td>
-      <td><?= $dish['descr_dish']?></td>
-      <td><?= $dish['ingredient']?></td>
-      <td><?= $dish['price']?> руб.</td>
-      <td><div class="chips chips-autocomplete"></div></td> 
-
-<?php
-}while($dish = mysql_fetch_array($dishQuery));
-// Освобождаем память от результата
-mysql_free_result($dishQuery);
-mysql_free_result($key_wordsQuery);
-?>
-  </table>
-</div>
-
-<div class="addDish">
-  <h2 class="center-align">Добавьте новое блюдо</h2>
-  <form action="save_dish.php" method="post" enctype = 'multipart/form-data'>
-    <div class="left-align input-group1">
-      <input type="text" value="" name="dish_name" placeholder="Наименование блюда"> 
-      <br>
-      <div class="fileLoad">
-        <input type="text"  id="Ssilka" value="" name="picture" placeholder="URL- Изображиеня" onkeyup="checkUrl()" >
-        <br>
-        <input type="file" id="filik" name="dish_pic" onchange="k()">
-        <label for="filik">
-          <i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
-        </label>
+<div class="container-fluid">
+  <div class="passport row">
+    <div class="content">
+      <h2 class="center-align zag2">Добро пожаловть, <?=$_SESSION['name']?> !</h2>
+      <div class="info col-md-9 col-sm-8">
+        Телефон: <?=$result['phone']?><br>
+        Сайт ресторана: <?=$result['site']?><br>
+        Директор: <?=$result['name_dir']?><br>
+        ИНН:<?=$result['inn']?>
+        ОГРН:<?=$result['ogrn']?>
+        р/с:<?=$result['rs']?><br>
+        <a href="statistic.php"><i class="fa fa-bar-chart" aria-hidden="true"></i>Статистика </a>
+      </div>
+      <div class="licence col-md-3 col-sm-4">
+        <img src="<?=$result['lic']; ?>">
       </div>
     </div>
-    <div class="center-align">
-      <input type="text"  value="" name="ingr" placeholder="Ингридиенты">
-      <br>
-      <input type="text"  value="" name="price" placeholder="Цена">
-    </div>
-    <textarea name="descr" placeholder="Описание"></textarea>
-    <div class="center-align">
-    <input type="submit"  value="Добавить блюдо">
-    </div>    
-  </form>  
+  </div>
 </div>
-   
+
+<div class="container-fluid">
+    <div class="menu row">
+        <div class="col-md-12">
+        <h2>Меню:</h2>
+        </div>
+        <div class="col-md-12 col-sm-12">
+        <table>
+          <tr>
+            <th>Наличие блюда</th>
+            <th>Наименование блюда</th>
+            <th>Фото</th>
+            <th>Описание</th>
+            <th>Ингридиенты</th>
+            <th>Цена</th>
+            <th>Ключевые слова</th>
+          </tr>
+
+          <?php do{?>
+          <tr>
+            <td><input type="checkbox" id="check" checked></td>
+            <td id="dish_name"><?=$dish['dish_name']?></td>
+            <td><img src="<?=$dish['icons']?>"></td>
+            <td><?= $dish['descr_dish']?></td>
+            <td><?= $dish['ingredient']?></td>
+            <td><?= $dish['price']?> руб.</td>
+            <td><div class="chips chips-autocomplete"></div></td> 
+
+          <?php
+          }while($dish = mysql_fetch_array($dishQuery));
+          // Освобождаем память от результата
+          mysql_free_result($dishQuery);
+          mysql_free_result($key_wordsQuery);
+          ?>
+          </tr>
+      </table>
+      </div>
+  </div>
+</div>
+
+<div class="container-fluid">
+  <div class="addDish row">
+    <div class="content">
+      <h2 class="center-align">Добавьте ваше новое блюдо</h2>
+      <form action="save_dish.php" method="post" enctype = 'multipart/form-data'>
+
+        <div class="col-md-4 col-sm-4">
+          <input type="text" value="" name="dish_name" placeholder="Наименование блюда"> 
+          <div class="fileLoad">
+              <input type="text"  id="Ssilka" value="" name="picture" placeholder="URL- Изображиеня" onkeyup="checkUrl()" >
+              <br>
+              <input type="file" id="filik" name="dish_pic" onchange="k()">
+              <label for="filik">
+                <i class="fa fa-plus-circle fa-4x" aria-hidden="true"></i>
+              </label>
+          </div>
+
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <input type="text"  value="" name="ingr" placeholder="Ингридиенты">
+          <input type="text"  value="" name="price" placeholder="Цена">
+          <input type="submit"  value="Добавить блюдо">
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <textarea name="descr" placeholder="Описание"></textarea>
+        </div>
+      </form>
+    </div>  
+  </div>
+</div>
+
+
+</div>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>   
 <script src="js/bootstrap.js"></script>
@@ -134,7 +159,7 @@ $(document).ready(function(){
               xmlhttp.open("GET","update.php?f=1",true);
               xmlhttp.send();
               console.log('мы поменяли твой флажок хозяин');
-              console.log($('#dish_name')[].value)
+              console.log($('#dish_name').value)
             }
             else{
               if (window.XMLHttpRequest) {
@@ -194,7 +219,7 @@ $(document).ready(function(){
   });*/
 
 </script>
-
+</div>
 </body>
 </html>
 
